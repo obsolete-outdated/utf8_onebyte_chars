9e3e0cf3f7932fc34d17a52e2f75a470786d5629
Moonspeaker <54249358+Moonspeaker@users.noreply.github.com>
2019-11-09 03:37:17 +0300

    Initial commit

---
+++ lib/utf8_onebyte_chars.dart
@@ -0,0 +1,4 @@
+library utf8_onebyte_chars;
+
+const String oneByteCharacters = "!\"#\$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwfxyz{|}~";
+const int oneByteCharacters_length = 95;
